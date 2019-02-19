a905e0ef309b719eb6b59773f4222f4058ab7b88
frankbyui <asb02001@byui.edu>
2019-02-19 03:09:06 -0700

    commented out java that refers to google maps

--- assignments/lesson-6/scripts/googlemap.js
+++ assignments/lesson-6/scripts/googlemap.js
@@ -1,10 +1,10 @@
 // JS for Google Map //
 
-function myMap(){
+/* function myMap(){
     var mapProp = {
 	    center: new google.maps.LatLng(42.0963, -111.8766),
 	    zoom: 8;
 	    };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
-
+*/
