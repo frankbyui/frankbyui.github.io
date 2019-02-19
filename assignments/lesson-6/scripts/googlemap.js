ce77aaa462d7bf62b4ff8b82b4b9d19b115b9b2d
frankbyui <asb02001@byui.edu>
2019-02-19 03:08:05 -0700

    remove google map

--- assignments/lesson-6/scripts/googlemap.js
+++ assignments/lesson-6/scripts/googlemap.js
@@ -3,7 +3,7 @@
 function myMap(){
     var mapProp = {
 	    center: new google.maps.LatLng(42.0963, -111.8766),
-	    zoom: 12;
+	    zoom: 8;
 	    };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
