a559b645c0eebb5e4a9c1695e359b77d988b639d
frankbyui <asb02001@byui.edu>
2019-02-19 17:10:58 -0700

    primary nav

--- assignments/lesson-6/scripts/togglenavmenu.js
+++ assignments/lesson-6/scripts/togglenavmenu.js
@@ -1,5 +1,5 @@
 // JS for toggling navigation menu //
 
 function toggleNavMenu() {
-    document.getElementById(nav).classList.toggle("hide");
+    document.getElementById(primarynav).classList.toggle("hide");
     }
