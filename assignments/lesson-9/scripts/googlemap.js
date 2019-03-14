20415eb6fb0bae4f72e08565f6c8f2b16b5f2a9a
frankbyui <asb02001@byui.edu>
2019-03-13 23:21:27 -0600

    lesson 9

---
+++ assignments/lesson-9/scripts/googlemap.js
@@ -0,0 +1,10 @@
+// JS for Google Map //
+
+/* function myMap(){
+    var mapProp = {
+	    center: new google.maps.LatLng(42.0963, -111.8766),
+	    zoom: 8;
+	    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+*/
