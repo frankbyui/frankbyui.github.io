7b822fdec1c9e9de435a99ad3f9c4603ac8c164a
frankbyui <asb02001@byui.edu>
2019-03-20 01:31:42 -0600

    lesson 10

---
+++ assignments/lesson-10/scripts/townJson.js
@@ -0,0 +1,83 @@
+// script for getting json data //
+
+var cityIndex, cities;
+var section =  document.querySelector("article");
+var requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+var request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+
+request.onload = function() {
+    var cityInfo = request.response;
+    showCityData(cityInfo);
+} 
+
+function showCityData(jsonObj) {
+    
+    cities = jsonObj["towns"];
+
+    for (cityIndex = 0; cityIndex < cities.length; cityIndex++){
+        if (cities[cityIndex].name == "Preston")
+        populateCityData(cities[cityIndex]);
+    }
+
+    for (cityIndex = 0; cityIndex < cities.length; cityIndex++){
+        if (cities[cityIndex].name == "Soda Springs")
+        populateCityData(cities[cityIndex]);
+    }
+
+    for (cityIndex = 0; cityIndex < cities.length; cityIndex++){
+        if (cities[cityIndex].name == "Fish Haven")
+        populateCityData(cities[cityIndex]);
+    }
+
+}
+
+function populateCityData(jsonObj) {
+
+    var city = jsonObj;
+
+    var myArticle = document.createElement("article");
+    var myH2 = document.createElement("h2");
+    var myH3 = document.createElement("h3");
+    var myPara1 = document.createElement("p");
+    var myPara2 = document.createElement("p");
+    var myPara3 = document.createElement("p");
+    var myImage = document.createElement("img");
+
+    myH2.textContent = city.name;
+    myH3.textContent = city.motto;
+    myPara1.textContent = "Year Founded: " + city.yearFounded;
+    myPara2.textContent = "Population: " + city.currentPopulation;
+    myPara3.textContent = "Average Rainfall: " + city.averageRainfall;
+
+    switch (myH2.textContent) {
+        case "Preston":
+            myImage.src = "images/homePreston.jpg";
+            myImage.id = "preston", alt = "Preston";
+            break;
+
+        case "Soda Springs":
+            myImage.src = "images/homeSoda.jpg";
+            myImage.id = "sodaSprings", alt = "Soda Sorings";
+            break;
+
+        case "Fish Haven":
+            myImage.src = "images/homeFish.jpg";
+            myImage.id = "fishHaven", alt = "Fish Haven";
+            break;
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myH3);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myImage);
+    myArticle.class= "homeTowns";
+
+    section.appendChild(myArticle);
+
+}
+
---
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -0,0 +1,13 @@
+/* Weather API Test Script */
+
+let weatherRequest = new XMLHttpRequest();
+apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=a814507b3b5b09de8c85f93d56908c48&mode=xml&units=imperial";
+
+weatherRequest.open("GET", apiURLstring, async="true");
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+    document.write(weatherData.temp)current-temp.innerHTML;
+}
