ebc7d4ace27bf1faf169fee3bd2338c4980821c7
frankbyui <asb02001@byui.edu>
2019-03-23 17:21:17 -0600

    weather data

--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -1,9 +1,10 @@
 /* Weather API Test Script */
 
 let weatherRequest = new XMLHttpRequest();
-apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=a814507b3b5b09de8c85f93d56908c48&mode=xml&units=imperial";
+var apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=a814507b3b5b09de8c85f93d56908c48&mode=xml&units=imperial";
 
 weatherRequest.open("GET", apiURLstring, async="true");
+weatherRequest.responseType = "json";
 weatherRequest.send();
 
 weatherRequest.onload =  function () {
