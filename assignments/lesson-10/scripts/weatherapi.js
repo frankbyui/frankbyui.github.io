795d90e09cb64110460221f1c5487f764094fb28
frankbyui <asb02001@byui.edu>
2019-03-23 23:48:30 -0600

    do it dangit

--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -1,7 +1,7 @@
 /* Weather API Test Script */
 
 var weatherRequest = new XMLHttpRequest();
-var apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=a814507b3b5b09de8c85f93d56908c48&mode=xml&units=imperial";
+var apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a814507b3b5b09de8c85f93d56908c48&units=imperial";
 
 weatherRequest.open("GET", apiURLstring, async="true");
 weatherRequest.send();
